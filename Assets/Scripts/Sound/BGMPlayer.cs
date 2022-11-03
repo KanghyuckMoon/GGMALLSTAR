3973d62f2b192a8bd9546e199d04a7c27ab6edaf
bossxz <moonkanghyuck@gmail.com>
2022-11-03 17:50:51 +0900

    11/03_브금 재생기 추가

--- Assets/Scripts/Sound/BGMPlayer.cs
+++ Assets/Scripts/Sound/BGMPlayer.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGMPlayer : MonoBehaviour
+namespace Sound
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BGMPlayer : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private bool _isPlayerOnAwake = false;
+        [SerializeField]
+        private AudioBGMType _audioBGMType = AudioBGMType.Count;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Start()
+        {
+            if (_isPlayerOnAwake)
+            {
+                PlayBGM();
+            }
+
+        }
+
+        private void PlayBGM()
+        {
+            SoundManager.Instance.PlayBGM(_audioBGMType);
+        }
     }
 }
