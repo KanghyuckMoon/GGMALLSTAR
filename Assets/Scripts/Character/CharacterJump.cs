5e6f02479cf57cc92eb177c62e2a98a13b2e43c5
hun1001 <l01022899266@gmail.com>
2022-11-08 13:51:04 +0900

    점프 제작중

--- Assets/Scripts/Character/CharacterJump.cs
+++ Assets/Scripts/Character/CharacterJump.cs
@@ -6,7 +6,7 @@ public class CharacterJump : CharacterComponent
 {
     public CharacterJump(Character character) : base(character)
     {
-        character.GetCharacterComponent<CharacterEvent>().AddEvent(EventKeyWord.UP, Jump);
+        character.GetCharacterComponent<CharacterEvent>().AddEvent(EventKeyWord.UP, null);
     }
 
     private bool _isJump = false;
@@ -14,15 +14,86 @@ public class CharacterJump : CharacterComponent
     protected override void Awake()
     {
         _isJump = false;
+        _rigidbody = Character.GetComponent<Rigidbody>();
     }
 
-    private void Jump()
+    #region Variables
+    protected Rigidbody _rigidbody = null;
+
+    [SerializeField]
+    protected float _jumpPower = 10f;
+
+    [SerializeField]
+    protected float _secondJumpPower = 0.8f;
+
+    [SerializeField]
+    private float _rayLength = 0.5f;
+
+    [SerializeField]
+    protected int _jumpCount = 2;
+    protected int _currentJumpCnt = 0;
+
+    private bool _isJumpable = true;
+    protected bool _isDoubleJump = false;
+    protected bool _isFirstJump = true;
+
+    protected bool _isGround = false;
+
+    [SerializeField]
+    private LayerMask _groundMask = 0;
+    #endregion
+
+    public override void Update()
     {
-        _isJump = !_isJump;
-        if (_isJump)
+        GroundCheck();
+    }
+
+    // private void Jump()
+    // {
+    //     _isJump = !_isJump;
+    //     if (_isJump)
+    //     {
+    //         return;
+    //     }
+
+    //     if (_isJumpable == false)
+    //         return;
+    //     if (_isGround == false && _currentJumpCnt >= _jumpCount)
+    //         return;
+
+    //     _isFirstJump = false;
+    //     float jumpPow = _currentJumpCnt > 0 ? _jumpPower * _secondJumpPower * accelerationJumpPower : _jumpPower * accelerationJumpPower;
+    //     if (_isDoubleJump)
+    //     {
+    //         jumpPow = _jumpPower * _secondJumpPower;
+    //     }
+
+    //     if (_currentJumpCnt == 0)
+    //     {
+    //         PaticleObj p = PoolManager.Instance.Pop("FirstJumpParticle") as PaticleObj;
+    //         p.transform.position = transform.position + Vector3.down * 0.1f;
+    //     }
+    //     else
+    //     {
+    //         PaticleObj p = PoolManager.Instance.Pop("JumpParticle") as PaticleObj;
+    //         p.transform.position = transform.position + Vector3.up * 0.1f;
+    //     }
+
+    //     OnJumpPress?.Invoke();
+
+    //     _rigid.velocity = new Vector2(_rigid.velocity.x, 0f);
+    //     _rigid.AddForce(Vector3.up * jumpPow, ForceMode2D.Impulse);
+
+    //     _isDoubleJump = false;
+    // }
+
+    private void GroundCheck()
+    {
+        _isGround = Physics.Raycast(Character.transform.position, Vector3.down, _rayLength, _groundMask);
+        _isFirstJump = _isGround;
+        if (_isGround)
         {
-            return;
+            _currentJumpCnt = 0;
         }
-        Debug.Log("Jump");
     }
 }
