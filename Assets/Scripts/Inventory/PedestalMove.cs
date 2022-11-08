1403e55691bdf14469125a0149b26dd3789d1b0f
moonkanghyuck <moonkanghyuck@gmail.com>
2022-11-08 21:04:07 +0900

    11/08_GotoGGM, 파우스트 출연

--- Assets/Scripts/Inventory/PedestalMove.cs
+++ Assets/Scripts/Inventory/PedestalMove.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class PedestalMove : MonoBehaviour
 {
+    public Vector3 rotation = Vector3.zero;
     public float speed = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        var angle = transform.eulerAngles;
         if (Input.GetKey(KeyCode.A))
 		{
-            angle.y += Time.deltaTime * speed;
+            rotation.y += Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            angle.y -= Time.deltaTime * speed;
+            rotation.y -= Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            angle.x += Time.deltaTime * speed;
+            rotation.x += Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            angle.x -= Time.deltaTime * speed;
+            rotation.x -= Time.deltaTime * speed;
         }
-        transform.eulerAngles = angle;
+        transform.eulerAngles = rotation;
     }
 }
