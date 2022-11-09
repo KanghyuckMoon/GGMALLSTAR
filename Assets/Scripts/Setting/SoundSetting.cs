87d9107a3c6b0b7202055fed1b97f42ecc3e4ae1
moonkanghyuck <moonkanghyuck@gmail.com>
2022-11-09 22:16:08 +0900

    11/09_소리 조절 기능 제작

--- Assets/Scripts/Setting/SoundSetting.cs
+++ Assets/Scripts/Setting/SoundSetting.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Setting
 {
     public class SoundSetting : MonoBehaviour
     {
+        [SerializeField] AudioMixer _audioMixer;
 
-    }
+		/// <summary>
+		/// 브금 크기 설정
+		/// </summary>
+		/// <param name="bgmValue"></param>
+		public void SetBgmAudio(float bgmValue)
+		{
+			_audioMixer.SetFloat("BGMVolume", Mathf.Log10(bgmValue) * 20);
+		}
+
+		/// <summary>
+		/// 효과음 크기 설정
+		/// </summary>
+		/// <param name="effValue"></param>
+		public void SetEffAudio(float effValue)
+		{
+			_audioMixer.SetFloat("EFFVolume", Mathf.Log10(effValue) * 20);
+		}
+	}
 }
