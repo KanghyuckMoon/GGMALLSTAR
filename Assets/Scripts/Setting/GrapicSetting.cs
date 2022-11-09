b8566ebe991c4512b8dc1fb615e721f693c06c0b
moonkanghyuck <moonkanghyuck@gmail.com>
2022-11-09 22:07:24 +0900

    11/09_그래픽 세팅 기능 제작

--- Assets/Scripts/Setting/GrapicSetting.cs
+++ Assets/Scripts/Setting/GrapicSetting.cs
@@ -1,11 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Setting
 {
 	public class GrapicSetting : MonoBehaviour
 	{
+		public Vector2[] resolutionList;
+
+		//그래픽 품질 설정
+		public void ChangeGrapicSetting(int index)
+		{
+			QualitySettings.SetQualityLevel(index, true);
+		}
+
+		//전체화면 설정
+		public void ChangeFullScreen(bool isFullScreen)
+		{
+			#if UNITY_EDITOR
+			EditorWindow.focusedWindow.maximized = isFullScreen;
+			#else
+				Screen.fullScreen = isFullScreen;
+			#endif
+		}
+
+		//해상도 설정
+		public void ChangeResolutionSetting(int index)
+		{
+			Screen.SetResolution((int)resolutionList[index].x, (int)resolutionList[index].y, Screen.fullScreen);
+		}
 
 	}
 }
