5a6a00852ee541cee915001a4f5782058f619826
moonkanghyuck <moonkanghyuck@gmail.com>
2022-11-10 14:19:02 +0900

    11/10_옵션 UI 제작

---
+++ Assets/Scripts/Setting/QuitGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Setting
+{
+	public class QuitGame : MonoBehaviour
+	{
+		public void Quit()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit(); // 어플리케이션 종료
+#endif
+		}
+	}
+}
