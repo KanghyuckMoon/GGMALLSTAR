6d28ff86257bac520fdc3322be8981cecb9c8666
hun1001 <l01022899266@gmail.com>
2022-11-14 23:19:00 +0900

    Mythical Dice 에셋 추가

--- Assets/Scripts/Base/SO/CharacterSO.cs
+++ Assets/Scripts/Base/SO/CharacterSO.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Character", menuName = "Character/New Character")]
 public class CharacterSO : ScriptableObject
 {
-    public CollaborationGame CollaborationGame;
     public string Name;
     public Sprite CharacterImage;
 
--- Assets/Scripts/Enum/CollaborationGame.cs
+++
@@ -1,7 +0,0 @@
-
-public enum CollaborationGame
-{
-    ORIGINAL,
-    IWBTJ,
-    Frog_in_the_well
-}
