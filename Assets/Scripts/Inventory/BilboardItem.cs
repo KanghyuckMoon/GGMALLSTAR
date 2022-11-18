4d32097e4f382aeaf8d9a06122137e58f57501e2
moonkanghyuck <moonkanghyuck@gmail.com>
2022-11-18 23:23:12 +0900

    11/18_빌보드 기능 수정

--- Assets/Scripts/Inventory/BilboardItem.cs
+++ Assets/Scripts/Inventory/BilboardItem.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class BilboardItem : MonoBehaviour
 {
+	public enum RotateAxis
+	{
+		Normal,
+		X,
+		Y,
+		Z,
+	}
+
+	public RotateAxis rotateAxis;
 	private Vector3 originRot;
 
 	private void Start()
 	{
-		originRot = transform.eulerAngles;
+		originRot = transform.localEulerAngles;
 	}
 
 	private void LateUpdate()
 	{
 		transform.LookAt(Camera.main.transform);
-		transform.eulerAngles += originRot;
+		Vector3 eulerRot = transform.localEulerAngles;
+		switch (rotateAxis)
+		{
+			case RotateAxis.Normal:
+				transform.localEulerAngles += originRot;
+				break;
+			case RotateAxis.X:
+				eulerRot.y = originRot.y;
+				eulerRot.z = originRot.z;
+				transform.localEulerAngles = eulerRot;
+				break;
+			case RotateAxis.Y:
+				eulerRot.x = originRot.x;
+				eulerRot.z = originRot.z;
+				transform.localEulerAngles = eulerRot;
+				break;
+			case RotateAxis.Z:
+				eulerRot.x = originRot.x;
+				eulerRot.y = originRot.y;
+				transform.localEulerAngles = eulerRot;
+				break;
+		}
 	}
 }
