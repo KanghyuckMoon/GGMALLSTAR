017eecb1898ac3ec636ce42de1d751c73bc3ce31
moonkanghyuck <moonkanghyuck@gmail.com>
2022-11-21 17:52:29 +0900

    11/21_비헤이비어트리 제작중

---
+++ Assets/Scripts/AI/BehaviourTree/BehaviourTree.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTree
+{
+
+}
+
+public class Sequence
+{
+
+}
+public class Selector
+{
+
+}
+public class Condition
+{
+
+}
+public class Inverter
+{
+
+}
+public class Loop
+{
+
+}
---
+++ Assets/Scripts/AI/BehaviourTree/Test/BehaviourTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTest
+{
+	private INode _rootNode;
+	private SelectorNode selectorNode;
+
+	public BehaviourTest()
+	{
+		INode[] nodes = new INode[5];
+		SelectorNode selectNode = new SelectorNode(nodes);
+	}
+
+	private void Move()
+	{
+
+	}
+
+	private void Attack()
+	{
+
+	}
+
+}
