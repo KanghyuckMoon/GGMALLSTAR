762a86ec4a7f05e5a3dbad304309a0a023c3a9f7
hun1001 <l01022899266@gmail.com>
2022-12-01 18:51:55 +0900

    대따 큰 제비 바람 발사

--- Assets/Scripts/Characters/Jaeby/CharacterSkill_Jaeby.cs
+++ Assets/Scripts/Characters/Jaeby/CharacterSkill_Jaeby.cs
@@ -8,60 +8,66 @@ using System;
 public class CharacterSkill_Jaeby : CharacterSkill
 {
 
-	public CharacterLevel CharacterLevel
-	{
-		get
-		{
-			characterLevel ??= Character.GetCharacterComponent<CharacterLevel>();
-			return characterLevel;
-		}
-	}
-
-	public override void Start()
-	{
-		base.Start();
+    public CharacterLevel CharacterLevel
+    {
+        get
+        {
+            characterLevel ??= Character.GetCharacterComponent<CharacterLevel>();
+            return characterLevel;
+        }
     }
 
-	public CharacterSkill_Jaeby(Character character) : base(character)
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public CharacterSkill_Jaeby(Character character) : base(character)
     {
         CharacterEvent.AddEvent(EventKeyWord.SKILL_1, () =>
         {
-            if(CharacterLevel.Level > 1 && skillCoolTime1 > Character.CharacterSO.skill1Delay)
-			{
-				skillCoolTime1 = 0f;
+            if (CharacterLevel.Level > 1 && skillCoolTime1 > Character.CharacterSO.skill1Delay)
+            {
+                skillCoolTime1 = 0f;
 
-				Character.GetCharacterComponent<CharacterAnimation>().SetAnimationTrigger(AnimationType.Skill1);
-				PoolManager.GetItem("Assets/Prefabs/SkillProjectile_Jaeby.prefab").GetComponent<SkillProjectile_Jaeby>().SetSkillProjectile(Character, Character.GetCharacterComponent<CharacterSprite>().Direction, Character.transform.position + new Vector3(0, 0.09f, 0), Character.HitBoxDataSO.hitBoxDatasList[1].hitBoxDatas[0]);
-			}
+                Character.GetCharacterComponent<CharacterAnimation>().SetAnimationTrigger(AnimationType.Skill1);
+                PoolManager.GetItem("Assets/Prefabs/SkillProjectile_Jaeby.prefab").GetComponent<SkillProjectile_Jaeby>().SetSkillProjectile(Character, Character.GetCharacterComponent<CharacterSprite>().Direction, Character.transform.position + new Vector3(0, 0.09f, 0), Character.HitBoxDataSO.hitBoxDatasList[1].hitBoxDatas[0]);
+            }
         }, EventType.KEY_DOWN);
 
         CharacterEvent.AddEvent(EventKeyWord.SKILL_2, () =>
         {
             if (CharacterLevel.Level > 2 && skillCoolTime2 > Character.CharacterSO.skill2Delay)
-			{
-				skillCoolTime2 = 0f;
+            {
+                skillCoolTime2 = 0f;
 
-				Sound.SoundManager.Instance.PlayEFF("JaebyAttack");
-				Character.GetCharacterComponent<CharacterAnimation>().SetAnimationTrigger(AnimationType.Skill2);
-				PoolManager.GetItem("Assets/Prefabs/SkillSpike_Jaeby.prefab").GetComponent<SkillSpike_Jaeby>().SetSkillSpike(Character, Character.transform.position + new Vector3(0, 0.09f, 0), Character.HitBoxDataSO.hitBoxDatasList[2].hitBoxDatas[0]);
-			}
+                Sound.SoundManager.Instance.PlayEFF("JaebyAttack");
+                Character.GetCharacterComponent<CharacterAnimation>().SetAnimationTrigger(AnimationType.Skill2);
+                PoolManager.GetItem("Assets/Prefabs/SkillSpike_Jaeby.prefab").GetComponent<SkillSpike_Jaeby>().SetSkillSpike(Character, Character.transform.position + new Vector3(0, 0.09f, 0), Character.HitBoxDataSO.hitBoxDatasList[2].hitBoxDatas[0]);
+            }
         }, EventType.KEY_DOWN);
-	}
 
-	public override void Update()
-	{
-		base.Update();
-		if (skillCoolTime1 < Character.CharacterSO.skill1Delay)
-		{
-			skillCoolTime1 += Time.deltaTime;
-			skill1CoolTimeChange?.Invoke();
-		}
+        CharacterEvent.AddEvent(EventKeyWord.ALL_STAR_SKILL, () =>
+        {
+            Debug.Log("ALL_STAR_SKILL");
+            PoolManager.GetItem("Assets/Prefabs/ALL_STAR_SKILL_Projectile_Jaeby.prefab").GetComponent<AllStarSkillProjectile_Jaeby>().SetSkillProjectile(Character, Character.GetCharacterComponent<CharacterSprite>().Direction, Character.transform.position + new Vector3(0, 0.09f, 0), null);
+        }, EventType.KEY_DOWN);
+    }
 
-		if (skillCoolTime2 < Character.CharacterSO.skill2Delay)
-		{
-			skillCoolTime2 += Time.deltaTime;
-			skill2CoolTimeChange?.Invoke();
-		}
+    public override void Update()
+    {
+        base.Update();
+        if (skillCoolTime1 < Character.CharacterSO.skill1Delay)
+        {
+            skillCoolTime1 += Time.deltaTime;
+            skill1CoolTimeChange?.Invoke();
+        }
 
-	}
+        if (skillCoolTime2 < Character.CharacterSO.skill2Delay)
+        {
+            skillCoolTime2 += Time.deltaTime;
+            skill2CoolTimeChange?.Invoke();
+        }
+
+    }
 }
---
+++ Assets/Scripts/Characters/Jaeby/Skill/AllStarSkillProjectile_Jaeby.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllStarSkillProjectile_Jaeby : MonoBehaviour
+{
+    private Character _character = null;
+    private Direction _direction = Direction.NONE;
+    private HitBoxData _hitBoxData;
+
+    public void SetSkillProjectile(Character character, Direction direction, Vector3 position, HitBoxData hitBoxData)
+    {
+        _character = character;
+        _direction = direction;
+        _hitBoxData = hitBoxData;
+        transform.position = position;
+        Debug.Log("SetSkillProjectile");
+
+        // if (_hitBoxData.atkEffSoundName != "")
+        // {
+        //     Sound.SoundManager.Instance.PlayEFF(_hitBoxData.atkEffSoundName);
+        // }
+
+        StartCoroutine(Move());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        Debug.Log(_character.gameObject);
+
+        if (other.gameObject == _character.gameObject)
+            return;
+
+        if (!other.gameObject.CompareTag(_character.tag))
+        {
+            CharacterAttack characterAttack = _character.GetCharacterComponent<CharacterAttack>();
+            characterAttack.TargetCharacterDamage = other?.gameObject?.GetComponent<Character>()?.GetCharacterComponent<CharacterDamage>();
+            //characterAttack.TargetCharacterDamage?.OnAttcked(null, _hitBoxData, other.ClosestPoint(transform.position), characterAttack.IsRight);
+
+            //AI
+            CharacterAIInput aiInput = characterAttack.Character.GetCharacterComponent<CharacterAIInput>();
+            if (aiInput is not null)
+            {
+                //aiInput.IsHit(_hitBoxData.actionName);
+            }
+
+            //Exp
+
+            // int expCount = (_hitBoxData.addExp / 5) + 1;
+
+            // for (int i = 0; i < expCount; ++i)
+            // {
+            //     StarEffect starEffect = Pool.PoolManager.GetItem("StarEff").GetComponent<StarEffect>();
+            //     starEffect.SetEffect(transform.position, _character.GetCharacterComponent<CharacterLevel>(), _hitBoxData.addExp / expCount);
+            // }
+        }
+
+        gameObject.SetActive(false);
+        Pool.PoolManager.AddObjToPool("Assets/Prefabs/ALL_STAR_SKILL_Projectile_Jaeby.prefab", gameObject);
+    }
+
+    private IEnumerator Move()
+    {
+        while (true)
+        {
+            switch (_direction)
+            {
+                case Direction.LEFT:
+                    transform.localScale = new Vector3(-5, 5, 1);
+                    transform.Translate(Vector3.left * 2f * Time.deltaTime);
+                    break;
+                case Direction.RIGHT:
+                    transform.localScale = new Vector3(5, 5, 1);
+                    transform.Translate(Vector3.right * 2f * Time.deltaTime);
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(SetActiveCoroutine());
+    }
+
+    private void OnDisable()
+    {
+        _character = null;
+        _direction = Direction.NONE;
+    }
+
+    private IEnumerator SetActiveCoroutine(bool active = false)
+    {
+        yield return new WaitForSeconds(5f);
+        gameObject.SetActive(active);
+        Pool.PoolManager.AddObjToPool("Assets/Prefabs/ALL_STAR_SKILL_Projectile_Jaeby.prefab", gameObject);
+    }
+}
