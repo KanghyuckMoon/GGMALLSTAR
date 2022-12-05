bb0877ff90fc0bd9005580a3b0741271740fc335
moonkanghyuck <moonkanghyuck@gmail.com>
2022-12-05 22:54:44 +0900

    12/05_크레딧 수정 현재 가진 것만 뜸

--- Assets/Scripts/UI/Credit/Credit.cs
+++ Assets/Scripts/UI/Credit/Credit.cs
@@ -5,18 +5,32 @@ using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+using Inventory;
 
 public class Credit : MonoBehaviour
 {
+	[SerializeField] private TextMeshProUGUI creditText;
 	[SerializeField] private RectTransform creditTransform;
 	[SerializeField] private float posY = 4000f;
 	[SerializeField] private float duration = 10f;
-	[SerializeField] private Dictionary<string, CreditData> creditDatas = new Dictionary<string, CreditData>();
+	[SerializeField] private List<GameObject> creditPrefebs = new List<GameObject>();
+	[SerializeField] private List<ItemDataSO> itemDataSOs = new List<ItemDataSO>();
 
-
-	private void Start()
+	private IEnumerator Start()
 	{
 		creditTransform.DOAnchorPosY(posY, duration).SetDelay(3f).OnComplete(() => MoveTitle());
+
+		yield return new WaitForSeconds(1f);
+
+		for(int i = 0; i < itemDataSOs.Count; ++i)
+		{
+			if(!InventoryStaticSO.itemDatas.Find(x => x.itemName == itemDataSOs[i].itemName))
+			{
+				//아이템 안 가지고 있음
+				creditPrefebs.Find(x => x.name == itemDataSOs[i].gameName)?.gameObject?.SetActive(false);
+				creditText.text = creditText.text.Replace(itemDataSOs[i].gameName, "???");
+			}
+		}
 	}
 
 	private void MoveTitle()
@@ -24,11 +38,4 @@ public class Credit : MonoBehaviour
 		SceneManager.LoadScene("Title");
 	}
 
-}
-
-[System.Serializable]
-public class CreditData
-{
-	public string creditName;
-	public GameObject gameObject;
 }
