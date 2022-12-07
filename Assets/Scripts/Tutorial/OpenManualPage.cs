9081868d21a9bc81bc298cf37d881f67dcee63ee
moonkanghyuck <moonkanghyuck@gmail.com>
2022-12-07 22:24:18 +0900

    12/07_튜토리얼 버튼 추가 및 메뉴얼버튼 추가

--- Assets/Scripts/Tutorial/OpenManualPage.cs
+++ Assets/Scripts/Tutorial/OpenManualPage.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenTutorialPage : MonoBehaviour
+public class OpenManualPage : MonoBehaviour
 {
 	private const string tutorialPage = "https://south-boot-7be.notion.site/GGMALLSTAR-5b56144548ef4bcfadb1e544d11c35c5";
 
-	public void OpenTutorial()
+	public void OpenManual()
 	{
 		Application.OpenURL(tutorialPage);
 	}
