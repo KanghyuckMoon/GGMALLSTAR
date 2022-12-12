0083d3ee8a00dcae1e74e58dc7c2ec581ab8af95
hun1001 <l01022899266@gmail.com>
2022-12-12 10:02:16 +0900

    파일 이름 변경, 캐릭터 기초 세팅

---
+++ Assets/Scripts/Characters/Puppet/Character_Puppet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Puppet : Character
+{
+    protected override void SetComponent()
+    {
+        base.SetComponent();
+    }
+}
