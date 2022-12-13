3666f0d461f0d70cedfa5fdba1500b8bab542789
moonkanghyuck <moonkanghyuck@gmail.com>
2022-12-13 09:44:56 +0900

    12/13_Info 텍스트 추가

--- Assets/Scripts/UI/InGame/RoundHUD.cs
+++ Assets/Scripts/UI/InGame/RoundHUD.cs
@@ -7,6 +7,7 @@ using DG.Tweening;
 
 public class RoundHUD : MonoBehaviour
 {
+	//RoundTexts
 	[SerializeField] private TextMeshProUGUI roundtext;
 	[SerializeField] private Image[] roundCountP1;
 	[SerializeField] private Image[] roundCountP2;
@@ -18,14 +19,22 @@ public class RoundHUD : MonoBehaviour
 	[SerializeField] private Material originMaterial;
 	[SerializeField] private Material negativeMaterial;
 
+	//Infos
+	[SerializeField] private GameObject p1Info;
+	[SerializeField] private GameObject p2Info;
+	[SerializeField] private TextMeshProUGUI[] p1InfoTexts;
+	[SerializeField] private TextMeshProUGUI[] p2InfoTexts;
+	[SerializeField] private InfoDataSO[] infoDatas;
+
+	//RoundText Coroutine Variables
 	private Vector2 originVector;
-
 	private RoundManager roundManager;
-
 	private Coroutine coroutine;
+
 
 	private void Awake()
 	{
+		//RoundText
 		roundManager = FindObjectOfType<RoundManager>();
 		roundManager.GameSetEvent += GameSet;
 		roundManager.RoundSetEvent += RoundSet;
@@ -33,13 +42,67 @@ public class RoundHUD : MonoBehaviour
 		roundManager.RoundStartEvent += RoundStart;
 		roundManager.RoundEndEvent += RoundEnd;
 		roundManager.GameEndEvent += GameEnd;
-
 		originVector = roundtext.rectTransform.anchoredPosition;
+
+		upperHud.gameObject.SetActive(false);
+		frameImage.gameObject.SetActive(true);
+
+		//Info
+		InfoSetting();
 	}
 	private void GameSet()
 	{
 		upperHud.gameObject.SetActive(false);
 		frameImage.gameObject.SetActive(true);
+		
+		//Info
+		StartCoroutine(GametSetInfo());
+	}
+
+	private void InfoSetting()
+	{
+		InfoDataSO infoDataSOP1 = null;
+		InfoDataSO infoDataSOP2 = null;
+		infoDataSOP1 = ReturnInfoData(SelectDataSO.characterSelectP1);
+		infoDataSOP2 = ReturnInfoData(SelectDataSO.characterSelectP2);
+
+		//P1 Info Text Setting
+		p1InfoTexts[0].text = infoDataSOP1.originGame;
+		p1InfoTexts[1].text = infoDataSOP1.homeGround;
+		p1InfoTexts[2].text = infoDataSOP1.numbering;
+
+		//P2 Info Text Setting
+		p2InfoTexts[0].text = infoDataSOP2.originGame;
+		p2InfoTexts[1].text = infoDataSOP2.homeGround;
+		p2InfoTexts[2].text = infoDataSOP2.numbering;
+
+	}
+
+	private InfoDataSO ReturnInfoData(CharacterSelect characterSelect)
+	{
+		switch (characterSelect)
+		{
+			default:
+			case CharacterSelect.Jaeby:
+				return infoDatas[0];
+			case CharacterSelect.Frog:
+				return infoDatas[1];
+			case CharacterSelect.Dice:
+				return infoDatas[2];
+		}
+	}
+
+	private IEnumerator GametSetInfo()
+	{
+		yield return new WaitForSeconds(0.2f);
+		p1Info.gameObject.SetActive(true);
+		p2Info.gameObject.SetActive(false);
+		yield return new WaitForSeconds(1f);
+		p1Info.gameObject.SetActive(false);
+		p2Info.gameObject.SetActive(true);
+		yield return new WaitForSeconds(1f);
+		p1Info.gameObject.SetActive(false);
+		p2Info.gameObject.SetActive(false);
 	}
 
 	private void RoundSet()
@@ -134,7 +197,7 @@ public class RoundHUD : MonoBehaviour
 		frameImage.gameObject.SetActive(true);
 	}
 
-	public IEnumerator SetText(string str)
+	private IEnumerator SetText(string str)
 	{
 		roundtext.fontMaterial = negativeMaterial;
 		yield return new WaitForSeconds(0.02f);
