b60c1770c1d3e198f4d59e6b6a245b39cf2b6946
moonkanghyuck <moonkanghyuck@gmail.com>
2022-12-13 09:09:29 +0900

    12/13_시간 텍스트와 라운드 텍스트 연출 및 폰트 변경

--- Assets/Scripts/UI/InGame/RoundHUD.cs
+++ Assets/Scripts/UI/InGame/RoundHUD.cs
@@ -18,7 +18,11 @@ public class RoundHUD : MonoBehaviour
 	[SerializeField] private Material originMaterial;
 	[SerializeField] private Material negativeMaterial;
 
+	private Vector2 originVector;
+
 	private RoundManager roundManager;
+
+	private Coroutine coroutine;
 
 	private void Awake()
 	{
@@ -29,6 +33,8 @@ public class RoundHUD : MonoBehaviour
 		roundManager.RoundStartEvent += RoundStart;
 		roundManager.RoundEndEvent += RoundEnd;
 		roundManager.GameEndEvent += GameEnd;
+
+		originVector = roundtext.rectTransform.anchoredPosition;
 	}
 	private void GameSet()
 	{
@@ -40,33 +46,89 @@ public class RoundHUD : MonoBehaviour
 	{
 		if (roundManager.RoundNumber == 5)
 		{
-			StartCoroutine(SetText("Round Final"));
+			CoroutineStop();
+			coroutine = StartCoroutine(RoundSetText("Round Final"));
 		}
 		else
 		{
-			StartCoroutine(SetText($"Round {roundManager.RoundNumber}"));
+			CoroutineStop();
+			coroutine = StartCoroutine(RoundSetText($"Round {roundManager.RoundNumber}"));
 		}
 		upperHud.gameObject.SetActive(true);
 		frameImage.gameObject.SetActive(false);
+
 	}
+
+	private IEnumerator RoundSetText(string text)
+	{
+		roundtext.rectTransform.DOKill();
+		roundtext.text = text;
+		roundtext.fontMaterial = negativeMaterial;
+		roundtext.rectTransform.localScale = Vector3.one * 2;
+		roundtext.gameObject.SetActive(true);
+		roundtext.rectTransform.DOShakePosition(0.2f, 10f);
+		yield return new WaitForSeconds(0.2f);
+		roundtext.fontMaterial = originMaterial;
+		roundtext.DOKill();
+		roundtext.rectTransform.DOScale(4f, 0.7f);
+		roundtext.rectTransform.anchoredPosition = originVector;
+		yield return new WaitForSeconds(0.7f);
+		roundtext.gameObject.SetActive(false);
+	}
+
 	private void RoundStart()
 	{
-		StartCoroutine(SetText($"GO"));
+		CoroutineStop();
+		coroutine = StartCoroutine(RoundStartText($"GO"));
 	}
+
+	private IEnumerator RoundStartText(string text)
+	{
+		roundtext.rectTransform.DOKill();
+		roundtext.text = text;
+		roundtext.rectTransform.localScale = Vector3.one * 20;
+		roundtext.gameObject.SetActive(true);
+		roundtext.rectTransform.DOScale(5f, 0.3f).SetEase(Ease.OutBack);
+		yield return new WaitForSeconds(0.1f);
+		roundtext.fontMaterial = negativeMaterial;
+		yield return new WaitForSeconds(0.05f);
+		roundtext.fontMaterial = originMaterial;
+		yield return new WaitForSeconds(0.1f);
+		roundtext.rectTransform.DOShakePosition(0.3f, 10f);
+		yield return new WaitForSeconds(0.3f);
+		roundtext.rectTransform.anchoredPosition = originVector;
+		yield return new WaitForSeconds(1f);
+		roundtext.gameObject.SetActive(false);
+	}
+
 	private void RoundEnd()
 	{
-		StartCoroutine(SetText($"KO"));
+		CoroutineStop();
+		coroutine = StartCoroutine(RoundEndText($"KO"));
 		CountImageSetting();
 		upperHud.gameObject.SetActive(false);
 		frameImage.gameObject.SetActive(true);
 	}
+
+	private IEnumerator RoundEndText(string text)
+	{
+		yield return null;
+		roundtext.rectTransform.DOKill();
+		roundtext.text = text;
+		roundtext.fontMaterial = originMaterial;
+		roundtext.rectTransform.localScale = Vector3.one * 2;
+		roundtext.rectTransform.DOScale(4f, 1f).OnComplete(() => roundtext.gameObject.SetActive(false));
+	}
+
 	private void RoundReady()
 	{
-		StartCoroutine(SetText($"Ready"));
+		CoroutineStop();
+		coroutine = StartCoroutine(SetText($"Ready"));
 	}
 	private void GameEnd()
 	{
-		StartCoroutine(SetText($"Game Set"));
+		CoroutineStop();
+		coroutine = StartCoroutine(RoundEndText($"Game Set"));
 		CountImageSetting();
 		upperHud.gameObject.SetActive(false);
 		frameImage.gameObject.SetActive(true);
@@ -84,7 +146,7 @@ public class RoundHUD : MonoBehaviour
 
 		roundtext.text = str;
 
-		roundtext.rectTransform.DOScale(2f, 1f).OnComplete(() => roundtext.gameObject.SetActive(false));
+		roundtext.rectTransform.DOScale(4f, 1f).OnComplete(() => roundtext.gameObject.SetActive(false));
 	}
 
 	private void CountImageSetting()
@@ -97,6 +159,14 @@ public class RoundHUD : MonoBehaviour
 		for (int i = 0; i < roundManager.WinCountP2; ++i)
 		{
 			roundCountP2[i].sprite = winSprite;
+		}
+	}
+
+	private void CoroutineStop()
+	{
+		if(coroutine != null)
+		{
+			StopCoroutine(coroutine);
 		}
 	}
 
