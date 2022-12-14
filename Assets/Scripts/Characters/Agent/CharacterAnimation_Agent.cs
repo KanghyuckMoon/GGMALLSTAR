07f0a6bee71efc47e9a593493e700d0d538321a5
hun1001 <l01022899266@gmail.com>
2022-12-14 21:19:32 +0900

    Agent기본 코드 추가

---
+++ Assets/Scripts/Characters/Agent/CharacterAnimation_Agent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation_Agent : CharacterAnimation
+{
+    public CharacterAnimation_Agent(Character character) : base(character)
+    {
+    }
+}
