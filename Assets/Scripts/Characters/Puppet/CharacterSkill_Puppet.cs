0bccd16e5959c5018556cea0d459d4a2267df5d5
moonkanghyuck <moonkanghyuck@gmail.com>
2022-12-18 23:24:05 +0900

    12/18_퍼펫 조정

--- Assets/Scripts/Characters/Puppet/CharacterSkill_Puppet.cs
+++ Assets/Scripts/Characters/Puppet/CharacterSkill_Puppet.cs
@@ -6,6 +6,15 @@ using Pool;
 
 public class CharacterSkill_Puppet : CharacterSkill
 {
+    public CharacterLevel CharacterLevel
+    {
+        get
+        {
+            characterLevel ??= Character.GetCharacterComponent<CharacterLevel>(ComponentType.Level);
+            return characterLevel;
+        }
+    }
+
     // Elemental's Transform
     private Transform _elementalTransform = null;
 
@@ -56,77 +65,150 @@ public class CharacterSkill_Puppet : CharacterSkill
             _elemental[i].SetActive(false);
         }
 
+
         CharacterEvent.AddEvent(EventKeyWord.SKILL_1, () =>
         {
-            _elementalType = _elementalType == ElementalType.Count ? ElementalType.Wind : (ElementalType)(((uint)_elementalType + 1) % (uint)ElementalType.Count);
-
-            for (int i = 0; i < _elemental.Length; i++)
+            if (CharacterLevel.Level > 1 && skillCoolTime1 >= Character.CharacterSO.skill1Delay)
             {
-                _elemental[i].SetActive(false);
+                skillCoolTime1 = 0f;
+
+                _elementalType = _elementalType == ElementalType.Count ? ElementalType.Wind : (ElementalType)(((uint)_elementalType + 1) % (uint)ElementalType.Count);
+
+                for (int i = 0; i < _elemental.Length; i++)
+                {
+                    _elemental[i].SetActive(false);
+                }
+
+                _elemental[(uint)_elementalType].SetActive(true);
+
+                // roll back stat
+
+
+                // Elemental Type Effect Setting
+                switch (_elementalType)
+                {
+                    case ElementalType.Wind:
+
+                        break;
+                    case ElementalType.Fire:
+
+                        break;
+                    case ElementalType.Earth:
+
+                        break;
+                    case ElementalType.Water:
+
+                        break;
+                    default:
+                        Debug.LogError("ElementalType Error");
+                        break;
+                }
+                Sound.SoundManager.Instance.PlayEFF("se_item_smartbomb_blink");
+                Skill1Action();
             }
-
-            _elemental[(uint)_elementalType].SetActive(true);
-
-            // roll back stat
-
-
-            // Elemental Type Effect Setting
-            switch (_elementalType)
-            {
-                case ElementalType.Wind:
-
-                    break;
-                case ElementalType.Fire:
-
-                    break;
-                case ElementalType.Earth:
-
-                    break;
-                case ElementalType.Water:
-
-                    break;
-                default:
-                    Debug.LogError("ElementalType Error");
-                    break;
-            }
-
         }, EventType.KEY_DOWN);
 
         CharacterEvent.AddEvent(EventKeyWord.SKILL_2, () =>
         {
-            switch (_elementalType)
+            if (CharacterLevel.Level > 2 && skillCoolTime2 >= Character.CharacterSO.skill2Delay)
             {
-                case ElementalType.Wind:
-                    var wind = PoolManager.GetItem("Assets/Prefabs/WindSkill.prefab").GetComponent<WindSkill>();
-                    wind.SetWindSkill(_character, _character.HitBoxDataSO.hitBoxDatasList[1].hitBoxDatas[0], _character.transform.position);
-                    Vector3 dir = new Vector3(_character.GetCharacterComponent<CharacterMove>().InputDirection.x * 5f, _character.GetCharacterComponent<CharacterMove>().InputDirection.y + 5f, 0f);
+                skillCoolTime2 = 0f;
 
-                    _character.Rigidbody.velocity = Vector3.zero;
-                    _character.Rigidbody.AddForce(dir, ForceMode.Impulse);
+                switch (_elementalType)
+                {
+                    case ElementalType.Wind:
+                        Sound.SoundManager.Instance.PlayEFF("se_common_swing_04");
+                        var wind = PoolManager.GetItem("Assets/Prefabs/WindSkill.prefab").GetComponent<WindSkill>();
+                        wind.SetWindSkill(_character, _character.HitBoxDataSO.hitBoxDatasList[1].hitBoxDatas[0], _character.transform.position);
+                        Vector3 dir = new Vector3(_character.GetCharacterComponent<CharacterMove>().InputDirection.x * 5f, _character.GetCharacterComponent<CharacterMove>().InputDirection.y + 5f, 0f);
 
-                    break;
-                case ElementalType.Fire:
-                    PoolManager.GetItem("Assets/Prefabs/FireBite.prefab").GetComponent<FireBiteSkill>().SetFireBiteSkill(_character, _character.HitBoxDataSO.hitBoxDatasList[2].hitBoxDatas[0], _character.transform.position + (_character.GetCharacterComponent<CharacterSprite>().Direction == Direction.RIGHT ? new Vector3(0.1f, 0.2f, 0f) : new Vector3(-0.1f, 0.2f, 0f)), _character.GetCharacterComponent<CharacterSprite>().Direction == Direction.RIGHT ? Vector3.right : Vector3.left);
-                    break;
-                case ElementalType.Earth:
-                    if (!_isEarthGolemSpawn)
-                    {
-                        _isEarthGolemSpawn = true;
-                        PoolManager.GetItem("Assets/Prefabs/EarthGolem.prefab").GetComponent<EarthGolem>().SetEarthGolem(_character, _character.HitBoxDataSO.hitBoxDatasList[3].hitBoxDatas[0], _character.transform.position + (_character.GetCharacterComponent<CharacterSprite>().Direction == Direction.RIGHT ? new Vector3(0.1f, 0.25f, 0f) : new Vector3(-0.1f, 0.25f, 0f)), _character.GetCharacterComponent<CharacterSprite>().Direction);
-                    }
-                    break;
-                case ElementalType.Water:
-                    PoolManager.GetItem("Assets/Prefabs/WaterBeam.prefab").GetComponent<WaterBeam>().SetWaterBeam(_character, _character.HitBoxDataSO.hitBoxDatasList[4].hitBoxDatas[0], _character.transform.position + (_character.GetCharacterComponent<CharacterSprite>().Direction == Direction.RIGHT ? new Vector3(0.5f, 0.125f, 0f) : new Vector3(-0.5f, 0.125f, 0f)), _character.GetCharacterComponent<CharacterSprite>().Direction);
-                    break;
-                default:
-                    Debug.LogError("ElementalType Error");
-                    break;
+                        _character.Rigidbody.velocity = Vector3.zero;
+                        _character.Rigidbody.AddForce(dir, ForceMode.Impulse);
+
+                        break;
+                    case ElementalType.Fire:
+                        Sound.SoundManager.Instance.PlayEFF("se_item_firebar_l");
+                        PoolManager.GetItem("Assets/Prefabs/FireBite.prefab").GetComponent<FireBiteSkill>().SetFireBiteSkill(_character, _character.HitBoxDataSO.hitBoxDatasList[2].hitBoxDatas[0], _character.transform.position + (_character.GetCharacterComponent<CharacterSprite>().Direction == Direction.RIGHT ? new Vector3(0.1f, 0.2f, 0f) : new Vector3(-0.1f, 0.2f, 0f)), _character.GetCharacterComponent<CharacterSprite>().Direction == Direction.RIGHT ? Vector3.right : Vector3.left);
+                        break;
+                    case ElementalType.Earth:
+                        Sound.SoundManager.Instance.PlayEFF("se_item_barrel_landing");
+                        if (!_isEarthGolemSpawn)
+                        {
+                            _isEarthGolemSpawn = true;
+                            PoolManager.GetItem("Assets/Prefabs/EarthGolem.prefab").GetComponent<EarthGolem>().SetEarthGolem(_character, _character.HitBoxDataSO.hitBoxDatasList[3].hitBoxDatas[0], _character.transform.position + (_character.GetCharacterComponent<CharacterSprite>().Direction == Direction.RIGHT ? new Vector3(0.1f, 0.25f, 0f) : new Vector3(-0.1f, 0.25f, 0f)), _character.GetCharacterComponent<CharacterSprite>().Direction);
+                        }
+                        break;
+                    case ElementalType.Water:
+                        Sound.SoundManager.Instance.PlayEFF("se_common_water_hit_l");
+                        PoolManager.GetItem("Assets/Prefabs/WaterBeam.prefab").GetComponent<WaterBeam>().SetWaterBeam(_character, _character.HitBoxDataSO.hitBoxDatasList[4].hitBoxDatas[0], _character.transform.position + (_character.GetCharacterComponent<CharacterSprite>().Direction == Direction.RIGHT ? new Vector3(0.5f, 0.125f, 0f) : new Vector3(-0.5f, 0.125f, 0f)), _character.GetCharacterComponent<CharacterSprite>().Direction);
+                        break;
+                    default:
+                        Debug.LogError("ElementalType Error");
+                        break;
+                }
+                Skill2Action();
             }
         }, EventType.KEY_DOWN);
 
         CharacterEvent.AddEvent(EventKeyWord.ALL_STAR_SKILL, () =>
         {
-            PoolManager.GetItem("Assets/Prefabs/FireBeamALLSTAR.prefab").GetComponent<FireBeamALLSTARSkill>().SetFireBeamALLSTARSkill(_character, _character.HitBoxDataSO.hitBoxDatasList[5].hitBoxDatas[0], _character.GetCharacterComponent<CharacterAttack>().TargetCharacterDamage.Character.transform.position); // != null ? _character.GetCharacterComponent<CharacterAttack>().TargetCharacterDamage.Character.gameObject.transform.position : _character.transform.position);
+            if (CharacterLevel.Level > 3 && !CharacterLevel.IsAllStarSkillUse)
+            {
+                isCanUseSkill3 = false;
+                characterLevel.IsAllStarSkillUse = true;
+                Sound.SoundManager.Instance.PlayEFF("se_common_boss_core_hit");
+                CameraManager.SetAllStar(Character.transform);
+                RoundManager.StaticSetInputSturnTime(1f);
+                RoundManager.StaticStopMove(1f);
+                AllStarSkillUse?.Invoke();
+                Character.StartCoroutine(AllStarSkill());
+                AllStarSkillAction();
+            }
         }, EventType.KEY_DOWN);
+    }
+
+
+    public override void Update()
+    {
+        base.Update();
+        if (skillCoolTime1 < Character.CharacterSO.skill1Delay)
+        {
+            skillCoolTime1 += Time.deltaTime;
+            skill1CoolTimeChange?.Invoke();
+            isCanUseSkill1 = false;
+        }
+        else if (CharacterLevel.Level > 1)
+        {
+            isCanUseSkill1 = true;
+        }
+
+        if (skillCoolTime2 < Character.CharacterSO.skill2Delay)
+        {
+            skillCoolTime2 += Time.deltaTime;
+            skill2CoolTimeChange?.Invoke();
+            isCanUseSkill2 = false;
+        }
+        else if (CharacterLevel.Level > 2)
+        {
+            isCanUseSkill2 = true;
+        }
+
+
+        if (!CharacterLevel.IsAllStarSkillUse && CharacterLevel.Level > 3)
+        {
+            isCanUseSkill3 = true;
+        }
+
+    }
+
+    private IEnumerator AllStarSkill()
+    {
+        yield return new WaitForSeconds(1f);
+
+        if (RoundManager.ReturnIsSetting())
+        {
+            Sound.SoundManager.Instance.PlayEFF("se_common_fire_ll_damage");
+            PoolManager.GetItem("Assets/Prefabs/FireBeamALLSTAR.prefab").GetComponent<FireBeamALLSTARSkill>().SetFireBeamALLSTARSkill(_character, _character.HitBoxDataSO.hitBoxDatasList[5].hitBoxDatas[0], _character.GetCharacterComponent<CharacterAttack>().TargetCharacterDamage.Character.transform.position); // != null ? _character.GetCharacterComponent<CharacterAttack>().TargetCharacterDamage.Character.gameObject.transform.position : _character.transform.position);
+        }
     }
 }
