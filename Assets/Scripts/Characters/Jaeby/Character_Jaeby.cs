7ecc74dfa71a2e3ba2862aa78d14ff3d4af87365
hun1001 <l01022899266@gmail.com>
2022-12-20 15:38:10 +0900

    스크립트 정리

--- Assets/Scripts/Characters/Jaeby/Character_Jaeby.cs
+++ Assets/Scripts/Characters/Jaeby/Character_Jaeby.cs
@@ -17,7 +17,6 @@ public class Character_Jaeby : Character
         AddComponent(ComponentType.Damage, new CharacterDamage(this));
         AddComponent(ComponentType.Jump, new CharacterJump(this));
         AddComponent(ComponentType.Level, new CharacterLevel(this));
-        // 스킬1당 스크립트 분리하기 안함
         AddComponent(ComponentType.Skill1, new CharacterSkill_Jaeby(this));
         AddComponent(ComponentType.Color, new CharacterColor_Jaeby(this));
         AddComponent(ComponentType.Debug, new CharacterDebug(this));
