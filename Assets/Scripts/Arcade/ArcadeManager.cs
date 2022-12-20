f0a119fd59ad4886801834aac119be5ec1bb3b70
moonkanghyuck <moonkanghyuck@gmail.com>
2022-12-20 15:41:02 +0900

    12/20_아케이드 정리

--- Assets/Scripts/Arcade/ArcadeManager.cs
+++ Assets/Scripts/Arcade/ArcadeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
@@ -13,14 +14,44 @@ namespace Arcade
 {
 	public class ArcadeManager : MonoBehaviour
 	{
-		[SerializeField] private Image characterImage;
-		[SerializeField] private RectTransform arrowImage;
-		[SerializeField] private RectTransform[] checkImages;
-		[SerializeField] private GameObject clearPanel;
-		[SerializeField] private GameObject endingButton;
+		[SerializeField, FormerlySerializedAs("characterImage")] 
+		private Image _characterImage;
+		[SerializeField, FormerlySerializedAs("arrowImage")] 
+		private RectTransform _arrowImage;
+		[SerializeField, FormerlySerializedAs("checkImages")] 
+		private RectTransform[] _checkImages;
+		[SerializeField, FormerlySerializedAs("clearPanel")] 
+		private GameObject _clearPanel;
+		[SerializeField, FormerlySerializedAs("endingButton")] 
+		private GameObject _endingButton;
 
-		private int clearRewardCount = 3;
+		private int _clearRewardCount = 3;
 
+		/// <summary>
+		/// 게임에서 승리시 아이템 박스를 3개 지급하고 3개 다 누르면 다음 게임으로 이동
+		/// </summary>
+		public void OpenItemBox()
+		{
+			_clearRewardCount--;
+			Sound.SoundManager.Instance.PlayEFF("se_item_genesis_get");
+			Inventory.InventoryManager.Instance.RandomGetItem();
+
+
+			if (_clearRewardCount == 0)
+			{
+				//Next Battle
+				if (SelectDataSO.winCount == 5)
+				{
+					_endingButton.SetActive(true);
+
+				}
+				else
+				{
+					_clearPanel.gameObject.SetActive(false);
+					GotoNextBattle();
+				}
+			}
+		}
 		private void Start()
 		{
 			SelectDataSO.isAICharacterP1 = false;
@@ -30,10 +61,10 @@ namespace Arcade
 			SceneManager.LoadScene("ItemPopUp", LoadSceneMode.Additive);
 
 			//Set Character Image and arrow image
-			var vec = arrowImage.anchoredPosition;
-			vec.x = checkImages[SelectDataSO.winCount].anchoredPosition.x;
-			arrowImage.anchoredPosition = vec;
-			characterImage.sprite = Addressable.AddressablesManager.Instance.GetResource<Sprite>($"{SelectDataSO.characterSelectP1.ToString()}_CImage");
+			var vec = _arrowImage.anchoredPosition;
+			vec.x = _checkImages[SelectDataSO.winCount].anchoredPosition.x;
+			_arrowImage.anchoredPosition = vec;
+			_characterImage.sprite = Addressable.AddressablesManager.Instance.GetResource<Sprite>($"{SelectDataSO.characterSelectP1.ToString()}_CImage");
 
 			//Victory
 			if (SelectDataSO.winCount > SelectDataSO.priviousWinCount)
@@ -48,45 +79,18 @@ namespace Arcade
 
 		}
 
-
-		/// <summary>
-		/// 게임에서 승리시 아이템 박스를 3개 지급하고 3개 다 누르면 다음 게임으로 이동
-		/// </summary>
-		public void OpenItemBox()
-		{
-			clearRewardCount--;
-			Sound.SoundManager.Instance.PlayEFF("se_item_genesis_get");
-			Inventory.InventoryManager.Instance.RandomGetItem();
-
-
-			if (clearRewardCount == 0)
-			{
-				//Next Battle
-				if (SelectDataSO.winCount == 5)
-				{
-					endingButton.SetActive(true);
-
-				}
-				else
-				{
-					clearPanel.gameObject.SetActive(false);
-					GotoNextBattle();
-				}
-			}
-		}
-
 		private void OpenClearPanel()
 		{
 			SelectDataSO.priviousWinCount = SelectDataSO.winCount;
-			clearRewardCount = 3;
-			clearPanel.gameObject.SetActive(true);
+			_clearRewardCount = 3;
+			_clearPanel.gameObject.SetActive(true);
 			Sound.SoundManager.Instance.PlayEFF("vc_narration_result_victory");
 		}
 
 		private void GotoNextBattle()
 		{
 			SelectDataSO.aiLevelP2 = SelectDataSO.winCount + 1;
-			arrowImage.DOAnchorPosX(checkImages[SelectDataSO.winCount + 1].anchoredPosition.x, 1f).OnComplete(() =>
+			_arrowImage.DOAnchorPosX(_checkImages[SelectDataSO.winCount + 1].anchoredPosition.x, 1f).OnComplete(() =>
 			{
 				int characterRandom = Random.Range(1, 7);
 				SelectDataSO.characterSelectP2 = (CharacterSelect)characterRandom;
